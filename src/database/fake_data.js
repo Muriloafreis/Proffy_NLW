1c8c52de6f3dda5ed352b09277ee855741d11a2c
Muriloafreis <muriloafreis@gmail.com>
2020-08-10 12:00:46 -0300

    Proffy_NLW

---
+++ src/database/fake_data.js
@@ -0,0 +1,36 @@
+// Dados
+const proffys = [
+    { 
+        name: "Lorena", 
+        avatar: "https://lh3.googleusercontent.com/NpM4mVkQ4OgHgn0XORVupYvDh06PqGZ2smtMl-Trg3dyGJVc-0UgmX_SaMx29ti01eppm7azLhJxeCIpzkkcxZluEtt6HXR2DK8Bcfj8HCkbLGXqW0euWS-BIiuHFSZ2miws-vB8j0s3F7Hbs_Ec3Bn3AUyAjhZ_64oreKgJMw5y4eLbQ7zfrNvp1Z5c9qToOaoIODhRxYi3yp5X8RMI2MacbasEtPo3GLBIu-K3NzRsBeLG2Ak12HHhyyKVY_Oje4A_TJnryOy66NuWSLrcXJKwjg2vmt9uOClGaClr-C5DVY6JSlX-xlmo5FRRCpo5SIy6INX5E3SRsgIX-BIYtRvA8QaoIzdqohM82_9oYBV9NcDwFODuH9fLdyDDQd9Ho6_J_XlfmhWm6TesTeKWI22a3HVlJYht-rqVSRRzvkXKEnmhLMcsUBSkYrg9S0Lcb3uk0EjpNWT8Dsw7Qf3bkFsiswLk95Q3N2c7cSuGUD5k-V2tLsmmuVOXClMiJC9w4kzGP2YHBOYfSXwx6p4Ojdea4IWWDE-w34s80wSPwhvhFhfRCUK_TsVTeyLVFGjB7qDrjUkg-yTmq3zuknJ5grKGJeFC5gNLIAEmnhQXuXRKpJabLk_X1GDt3BiZGEP1tHftnAYbpnwhtORkS1u8Spqm0plIcT258MNhq9vF6KtraA2ipBAdB_sCuRFt=w715-h953-no?authuser=1",
+        whatsapp: "22997103174" ,
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonada por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Ciências"  ,
+        cost: "200",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220],
+    },
+    { 
+    name: "Nory", 
+    avatar: "https://photos.google.com/u/1/search/_cAF1QipNaXeHnk8F4o6-mW~uO-rxk1yblrr7mzQ4M_/photo/AF1QipOdbPJk8XQp3Tfg03gw-52Xgk-kVIvrb0Dio6Rn",
+    whatsapp: "11967108332" ,
+    bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    subject: "Química"  ,
+    cost: "20",
+    weekday: [1],
+    time_from: [720],
+    time_to: [1220],
+    },
+    {
+    name: 'Kell',
+    avatar: 'https://photos.google.com/u/1/search/_cAF1QipPA0scsbCSQBTyNtBaOmP2zD1fmEodPjbU_/photo/AF1QipO7_Md7W2JkzuvqTli3IhYpgEAomniC3orkyWl6',
+    whatsapp: '11958796913',
+    bio: 'Instrutor de Educação Física, joga bola todo final de semana, sabe andar de skate e fuma maconha.',
+    subject: "Educação Física",
+    cost: "50",
+    weekday: [2],
+    time_from: [720],
+    time_to: [1220]
+    }
+]
